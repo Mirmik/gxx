91ddca52cb2d780e16e132cd8e20e9c581aa807a
mirmik <mirmikns@yandex.ru>
2017-09-21 12:33:41 +0300

    a

--- gxx/io/istream.h
+++ gxx/io/istream.h
@@ -4,6 +4,15 @@
 namespace gxx {
 	namespace io {
 		class istream {
+			virtual int read(char* str, size_t sz) {
+				return readData(str, sz);
+			}
+
+			virtual int getchar() {
+				char c;
+				read(&c,1);
+				return c;
+			}
 
 		protected: 
 			virtual int readData(char* str, size_t sz) = 0;
