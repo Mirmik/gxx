d85c6cc6a7e55cedf4c757b90aef4227559fdd81
Mirmik <mirmikns@yandex.ru>
2017-11-03 12:16:19 +0300

    a

---
+++ gxx/terminal/terminal.h
@@ -0,0 +1,73 @@
+#ifndef GXX_TERMINAL_H
+#define GXX_TERMINAL_H
+
+#include <gxx/debug/dprint.h>
+#include <gxx/io/iostream.h>
+#include <gxx/panic.h>
+#include <gxx/line.h>
+
+namespace gxx {
+	class terminal {
+		gxx::io::ostream& out;
+		gxx::line line;
+
+	public:
+		terminal(gxx::io::ostream & out) : out(out) {}
+
+		uint8_t state = 0;
+		uint8_t echo = true;
+
+		int arg = 0;
+
+		void newline() {
+
+		}
+
+		void start() {
+			out.print("input> ");
+		}
+
+		void newchar(char c) {
+			switch (state) {
+				case 0: 
+					switch(c) {
+						case '\r': 
+							return;
+						case '\n': 
+							newline();
+							return;
+						case 0x28:
+							state = 1;
+							return;						
+						default:
+							if (line.putchar(c)) out.putchar(c);
+							return;
+					}
+				case 1:
+					if (c == '[') {
+						arg = 0;
+						state = 2;
+					}
+					else state = 0;
+					break;
+				case 2:
+					if (isdigit(c)) { 
+						arg = arg * 10 + (c - '0');
+						return;
+					}
+					switch (c) {
+						case 'A' : dprln("up"); break;
+						case 'B' : dprln("down"); break;
+						case 'C' : dprln("C!!"); break;
+						case 'D' : dprln("D!!"); break;
+					}
+					state = 0;
+
+				default:
+					gxx::panic("terminal, default state");
+			}
+		}
+	};
+}
+
+#endif
---
+++ tests/terminal/main.cpp
@@ -0,0 +1,9 @@
+#include <gxx/terminal/terminal.h>
+#include <gxx/debug/debug_ostream.h>
+
+gxx::debug_ostream dout;
+gxx::terminal terminal(dout);
+
+int main() {
+
+}
