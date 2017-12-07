5d05226b43bdecee5bfedb35892487e03436087d
mirmik <mirmikns@yandex.ru>
2017-12-07 15:01:00 +0300

    a

--- gxx/debug/delay.h
+++ gxx/debug/delay.h
@@ -6,11 +6,15 @@
 
 __BEGIN_DECLS
 
-//void debug_delay(uint32_t ms);
+static double __debug_delay_multiplier = 1;
 
 static inline void debug_simple_delay(uint64_t ticks) {
 	volatile uint64_t count = ticks;
 	while(count--);
+}
+
+static void debug_delay(uint32_t ms) {
+	debug_simple_delay(ms * __debug_delay_multiplier);
 }
 
 __END_DECLS
