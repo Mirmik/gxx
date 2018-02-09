31b38207cf71ca7439119fff091ca9a8abcfdef8
mirmik <mirmikns@yandex.ru>
2018-02-09 13:53:03 +0300

    arr

--- gxx/datastruct/array.h
+++ gxx/datastruct/array.h
@@ -4,7 +4,7 @@
 #include <gxx/util/decltypeof.h>
 #include <gxx/util/macro.h>
 
-#define __array_for_each(it,arr,sz,end) for(decltypeof(arr)* it = arr, *end = arr + sz; it != end; ++it)
+#define __array_for_each(it,arr,sz,end) for(decltypeof(arr) it = arr, end = arr + sz; it != end; ++it)
 #define array_for_each(it,arr,sz) __array_for_each(it,arr,sz,MACRO_GUARD(end))
 
 #endif
