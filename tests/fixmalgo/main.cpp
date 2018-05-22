de62bbd3f11029252c6c9a8ba03618c1f9991b0c
Mirmik <mirmikns@yandex.ru>
2018-05-22 15:18:39 +0300

    a

--- tests/fixmalgo/main.cpp
+++ tests/fixmalgo/main.cpp
@@ -1,8 +1,8 @@
 #include <gxx/math/linalg.h>
-#include <gxx/print.h>
-#include <gxx/print/linalg.h>
+//#include <gxx/print.h>
+//#include <gxx/print/linalg.h>
 
-using namespace gxx::linalg;
+using namespace linalg;
 
 constexpr int f(int a, int b) { return a+b; }
 
@@ -14,15 +14,19 @@ int main() {
 	constexpr vec<int,2> a {1,2};
 	constexpr vec<int,2> b {4,3};
 
-	constexpr mat<int,2,2> m{a,b}; 
+	constexpr vec<int,2> c = linalg::abs(a);
+
+	//constexpr mat<int,2,2> m{a,b}; 
 	//constexpr vec<int,2> m2 = zip(a, b, f); 
 	//constexpr vec<int,2> m2 = zip(a, b, std::plus<int>());
 	//constexpr vec<int,2> m2 = a + b;
 
 
-	constexpr auto m2 = (m * m - m) * mat<int,2,2>();
+	//constexpr mat<int,2,2> m2 = (m * m - m) * mat<int,2,2>();
 
-	gxx::println(m2);
+	//constexpr mat<int,2,2> m3 = linalg::abs(m2);	
+
+	//gxx::println(m2);
 
 	//constexpr mat<double,2,2> m {a,b};
 
