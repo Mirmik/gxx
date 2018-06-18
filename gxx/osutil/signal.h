040b759009fea74cbbc1beb3236384ac57171b81
mirmik <mirmikns@yandex.ru>
2018-06-18 12:18:04 +0300

    s

--- gxx/osutil/signal.h
+++ gxx/osutil/signal.h
@@ -7,15 +7,8 @@
 
 namespace gxx {
 	namespace osutil {
-		void setsig(int fd, int sig) {
-			fcntl(fd, F_SETOWN, getpid());
-			fcntl(fd, F_SETSIG, SIGUSR1);
-			fcntl(fd,F_SETFL,fcntl(fd,F_GETFL) | O_NONBLOCK | O_ASYNC); 
-   		}
-
-   		void signal(int sig, void(*handler)(int)) {
-   			::signal(sig, handler);
-   		}
+		void setsig(int fd, int sig);
+   		void signal(int sig, void(*handler)(int));
 	}
 }
 
--- gxx/osutil/src/posix.cpp
+++ gxx/osutil/src/posix.cpp
@@ -9,8 +9,16 @@ int gxx::osutil::nonblock(int fd, bool en) {
    return fcntl(fd, F_SETFL, flags) == 0;
 }
 
-/*int gxx::osutil::nodelay(int fd, bool en) {
-	int on = en;
-	int rc = setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, (char *) &on, sizeof(on));
-	return rc;
-}*/
+namespace gxx {
+	namespace osutil {
+		void setsig(int fd, int sig) {
+			fcntl(fd, F_SETOWN, getpid());
+			fcntl(fd, F_SETSIG, SIGUSR1);
+			fcntl(fd,F_SETFL,fcntl(fd,F_GETFL) | O_NONBLOCK | O_ASYNC); 
+   		}
+
+   		void signal(int sig, void(*handler)(int)) {
+   			::signal(sig, handler);
+   		}
+	}
+}
