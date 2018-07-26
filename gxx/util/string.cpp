75c23dbbb17147b04ec4aa631f86b7dfe6a54268
mirmik <mirmikns@yandex.ru>
2018-07-26 14:12:48 +0300

    a

--- gxx/util/string.cpp
+++ gxx/util/string.cpp
@@ -45,4 +45,29 @@ namespace gxx {
 
 		return ret;
 	}  
+	
+	std::string dstring(const void* data, size_t size) {
+		std::string ret;
+		char* it = (char*) data;
+		char* eit = it + size;
+
+		for (;it!=eit;++it) {
+			if (isprint(*it)) ret.push_back(*it);
+			else if (*it == '\n') ret.append("\\n", 2);
+			else if (*it == '\t') ret.append("\\t", 2);
+			else if (*it == '\\') ret.append("\\\\", 2);
+			else { 
+				uint8_t hi = byte2sym((*it & 0xF0) >> 4);
+				uint8_t low = byte2sym(*it & 0x0F);
+				ret.append("\\x", 2);
+				ret.push_back(hi);  
+				ret.push_back(low); 
+			}
+		}
+		return ret;
+	}
+
+	std::string dstring(gxx::buffer buf) {
+		return dstring(buf.data(), buf.size());
+	}	
 }
--- gxx/util/string.h
+++ gxx/util/string.h
@@ -2,10 +2,12 @@
 #define GXX_STRING_H
 
 #include <cstdint>
-#include <gxx/util/asciiconvert.h>
 #include <list>
 #include <vector>
 #include <string>
+
+#include <gxx/buffer.h>
+#include <gxx/util/asciiconvert.h>
 
 namespace gxx {
 	using strvec = std::vector<std::string>;
@@ -28,26 +30,9 @@ namespace gxx {
 		return ret;
 	}
 
-	static inline std::string dstring(const void* data, size_t size) {
-		std::string ret;
-		char* it = (char*) data;
-		char* eit = it + size;
-
-		for (;it!=eit;++it) {
-			if (isprint(*it)) ret.push_back(*it);
-			else if (*it == '\n') ret.append("\\n", 2);
-			else if (*it == '\t') ret.append("\\t", 2);
-			else if (*it == '\\') ret.append("\\\\", 2);
-			else { 
-				uint8_t hi = byte2sym((*it & 0xF0) >> 4);
-				uint8_t low = byte2sym(*it & 0x0F);
-				ret.append("\\x", 2);
-				ret.push_back(hi);  
-				ret.push_back(low); 
-			}
-		}
-		return ret;
-	}
+	std::string dstring(const void* data, size_t size);
+	std::string dstring(gxx::buffer buf);
+	std::string dstring(const std::string& str);
 }
 
 #endif
