1b0de1c77e92470fa345563a49d69e9a5c6c06dc
mirmik <mirmikns@yandex.ru>
2018-08-17 15:34:38 +0300

    a

--- gxx/sshell.h
+++ gxx/sshell.h
@@ -2,21 +2,53 @@
 #define GXX_SIMPLE_SHELL_H
 
 #include <gxx/datastruct/argvc.h>
+
+#define SSHELL_ARGCMAX 10
+
+#define SSHELL_OK 0
+#define SSHELL_EMPTY_STRING -1
+#define SSHELL_FUNCTION_NOT_EXIST -2
+
+struct sshell_command {
+	const char* name;
+	int (*func) (int, char**);
+};
+
+int sshell_execute(char* str, const sshell_command* cmd, int cmdlen, int* retptr) {
+	char* argv[SSHELL_ARGCMAX];
+	int argc;
+	int res;
+
+	if (*str == '\0') {
+		return SSHELL_EMPTY_STRING;
+	}
+
+	argc = argvc_internal_split(str, argv, SSHELL_ARGCMAX);
+
+	for(int i = 0; i < cmdlen; ++i) {
+		if (!strcmp(argv[0], cmd[i].name)) {
+			res = cmd[i].func(argc, argv);
+			if (retptr) *retptr = res;
+			return SSHELL_OK;
+		}
+	}
+
+	return SSHELL_FUNCTION_NOT_EXIST;
+}
+
+int sshell_execute_safe(const char* str, const sshell_command* cmd, int cmdlen, int* retptr) {
+	char locstr[strlen(str) + 1];
+	strcpy(locstr, str);
+	return sshell_execute(locstr, cmd, cmdlen, retptr);
+}
+
+
+
+#ifdef __cplusplus
 #include <gxx/event/delegate.h>
 
 namespace gxx{
-
-	/*class sshell_record {
-	public:
-		hlist_node hlnk;
-		const char* name;
-		delegate<int, int, char**> dlg;
-
-		sshell_record(const char* name, delegate<int, int, char**> dlg) :
-			name(name), dlg(dlg) { }
-		static const char*& getkey(sshell_record& rec) { return rec.name; }
-	};*/
-	struct sshell {
+	struct [[deprecated]] sshell {
 		struct command {
 			const char* name;
 			int (*func) (int, const char**);
@@ -29,40 +61,10 @@ namespace gxx{
 			table = tbl;
 			table_size = size;
 		}
-
-	//public:
-		//gxx::static_hashtable<10, sshell_record, const char*, &sshell_record::hlnk> table;
-
-	//public:
 		static constexpr uint8_t OK = 0;
 		static constexpr uint8_t FunctionNotExist = -1;
 		static constexpr uint8_t EmptyString = -2;
-//		static constexpr uint8_t WrongArgsTotal = -3;
-//		static constexpr uint8_t WrongArgsData = -4;
-//		static constexpr uint8_t InternalError = -5;
 
-	/*	void add(const char* name, delegate<int, int, char**> dlg) {
-			auto node = new sshell_record(name, dlg);
-			table.put(*node);
-		};
-
-		void add(const char* name, delegate<int> vdlg) {
-			delegate<int, int, char**> dlg;
-			memcpy(&dlg, &vdlg, sizeof(vdlg));
-			auto node = new sshell_record(name, dlg);
-			table.put(*node);
-		};
-
-		void add(const char* name, int(*func)()) {
-			auto node = new sshell_record(name, gxx::delegate<int, int, char**>(reinterpret_cast<int(*)(int, char**)>(func)));
-			table.put(*node);
-		};
-
-		void add(const char* name, int(*func)(int, char**)) {
-			auto node = new sshell_record(name, gxx::delegate<int, int, char**>(func));
-			table.put(*node);
-		};
-*/
 		int __execute_nosafe(char* str, int* ret) {
 			str_argvc_t str_argvc(str);
 
@@ -96,26 +98,8 @@ namespace gxx{
 			str[len] = 0;
 			return __execute_nosafe(str, ret);
 		}
-
-/*		static const char* strerr(int retcode) {
-			switch (retcode) {
-				case OK:
-					return "RetCodeOK";
-				case EmptyString:
-					return "EmptyString";
-				case FunctionNotExist:
-					return "FunctionNotExist";
-				case WrongArgsTotal:
-					return "WrongArgsTotal";
-				case WrongArgsData:
-					return "WrongArgsData";
-				case InternalError:
-					return "InternalError";
-				default:
-					return "UnregistredRetCode";
-			};
-		};*/
 	};
 }
+#endif
 
 #endif
--- tests/sshell/main.cpp
+++ tests/sshell/main.cpp
@@ -4,25 +4,28 @@
 #include <gxx/sshell.h>
 #include <gxx/debug/dprint.h>
 
-int hello_world() {
+int hello(int argc, char**) {
 	dprln("HelloWorld");
 }
 
-gxx::sshell shell;
+sshell_command cmd[] = {
+	{ "hello", hello }
+};
 
 void read_func() {
 	while(1) {
 		int sts;
 		std::string str;
 		std::getline(std::cin, str);
-		shell.execute(str, &sts);
-		if (sts != 0) {
-			dprln(shell.strerr(sts));
-		}
+
+		sshell_execute_safe(str.c_str(), cmd, sizeof(cmd)/sizeof(sshell_command), &sts);
+		//shell.execute(str, &sts);
+		//if (sts != 0) {
+		//	dprln(sshell_strerr(sts));
+		//}
 	}
 }
 
 int main() {
-	shell.add("hello", hello_world);
 	read_func();
 }
