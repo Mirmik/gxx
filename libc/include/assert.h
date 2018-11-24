416507afc263aa27ee8a6f711c246e6020b987e9
mirmik <mirmikns@yandex.ru>
2018-11-24 23:30:40 +0300

    a

--- libc/include/assert.h
+++ libc/include/assert.h
@@ -4,7 +4,7 @@
 #include <gxx/debug/dprint.h>
 
 static inline void __assert(const char* message, const char* file, int line) { dprln("assert: "); DPRINT(message); DPRINT(file); DPRINT(line); while(1); }
-#define assert(EX) if (!EX) __assert (#EX, __FILE__, __LINE__)
+#define assert(EX) if (!(EX)) __assert (#EX, __FILE__, __LINE__)
 
 #ifndef __cplusplus
 #	define static_assert(a,b) _Static_assert(a,b)
