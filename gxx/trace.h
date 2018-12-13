81c89bf86b3cd471495f0b1936e3d444e993316c
Mirmik <mirmikns@yandex.ru>
2018-12-13 16:54:08 +0300

    trace

--- gxx/trace.h
+++ gxx/trace.h
@@ -3,6 +3,7 @@
 
 //#include <gxx/log/logger2.h>
 #include <gxx/print.h>
+#include <gxx/print/stdprint.h>
 #include <gxx/arglist.h>
 
 using namespace gxx::argument_literal;
@@ -17,6 +18,7 @@ namespace gxx {
 
 struct tracer {
     const char* func = func;
+    std::string ret;
 
     tracer(const char* func) {
         this->func = func;
@@ -27,16 +29,26 @@ struct tracer {
 
     ~tracer() {
         --trace_level;
-        dpr("TRACE: "); dpr(trace_level); dpr(": <- "); dprln(func);
+        dpr("TRACE: "); dpr(trace_level); dpr(": <- "); dpr(func);
+        if (ret != "") 
+        {
+            dpr(":"); gxx::println(ret); 
+        } 
+        else 
+        {
+            dprln();
+        }
     }
 };
 }
 
 #if defined(NOTRACE) && NOTRACE!=0
 #define TRACE()
+#define TRRET(ex) ex
 #define TRACE_ARGS(...)
 #else
 #define TRACE() gxx::tracer __tracer(__PRETTY_FUNCTION__); dln();
+#define TRRET(ex) ({ auto __ret = (ex); __tracer.ret = std::to_string(__ret); return __ret; __ret; })
 #define TRACE_ARGS(...) gxx::tracer __tracer(__PRETTY_FUNCTION__); dpr(" with "); gxx::println(__VA_ARGS__);
 #endif
 
